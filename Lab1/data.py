02a2776a394023688382692ad3218581b75be190
Esteban Márquez Delgado <emarquez1895@gmail.com>
2022-09-05 19:39:26 -0500

    Lab 1 Advances

---
+++ Lab1/data.py
@@ -0,0 +1,11 @@
+"""
+# -- --------------------------------------------------------------------------------------------------- -- #
+# -- project: A SHORT DESCRIPTION OF THE PROJECT                                                         -- #
+# -- script: data.py : python script for data collection                                                 -- #
+# -- author: Márquez Delgado Esteban                                                                     -- #
+# -- license: GPL-3.0 License                                                                            -- #
+# -- repository: https://github.com/EstebanMqz/Microstructures                                           -- #
+# -- --------------------------------------------------------------------------------------------------- -- #
+"""
+
+
